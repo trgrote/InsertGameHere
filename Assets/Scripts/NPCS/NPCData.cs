ddb2fe2707b4067ae21472f76d4ebf12e24b8f87
Taylor Grote <grotese@gmail.com>
2018-06-02 16:19:22 -0500

    Add NPC Data

---
+++ Assets/Scripts/NPCS/NPCData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Just the facts about this NPC
+public class NPCData : MonoBehaviour 
+{
+	[HideInInspector]
+	public float tanLevel;
+}
