8494ea2e22e2f147fbfce6401a96de1b79482078
Taylor Grote <grotese@gmail.com>
2018-06-02 16:24:10 -0500

    Add NPC Logic

---
+++ Assets/Scripts/NPCS/NPCLogic.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// All the thinking and desires the NPCS have
+[RequireComponent(typeof(NPCData))]
+public class NPCLogic : MonoBehaviour 
+{
+	NPCData data;
+
+	void Start()
+	{
+		data = GetComponent<NPCData>();
+	}
+}
