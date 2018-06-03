908c11b4d6f3b44f2876649fda1428534e182cda
Taylor Grote <grotese@gmail.com>
2018-06-03 11:58:26 -0500

    Add NPC Spawner

---
+++ Assets/Scripts/NPCSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSpawner : MonoBehaviour 
+{
+
+	// Use this for initialization
+	[SerializeField] GameObject prefab;
+
+	[SerializeField] float timeBetweenSpawn = 10f;
+
+	public void Spawn()
+	{
+		Instantiate(prefab);
+	}
+
+	void Start()
+	{
+		StartCoroutine(ConstantSpawn());
+	}
+
+	IEnumerator ConstantSpawn()
+	{
+		while (true)
+		{
+			Spawn();
+			yield return new WaitForSeconds(timeBetweenSpawn);
+		}
+	}
+}
