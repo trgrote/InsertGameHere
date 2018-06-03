75bc732a973973ab0c057f971347a97e8364222b
Taylor Grote <grotese@gmail.com>
2018-06-03 16:35:31 -0500

    Add configurable camera speed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 // Simple Camera Controller that just moves the camera left or right
 public class CameraController : MonoBehaviour 
 {
+	[SerializeField] float cameraSpeed = 2f;
+
 	void Update()
 	{
 		var horiz = Input.GetAxis("Horizontal");
 		if (Mathf.Abs(horiz) > 0)
 		{
-			transform.position += horiz * transform.right * 2;
+			transform.position += horiz * transform.right * cameraSpeed;
 		}
 	}
 }
