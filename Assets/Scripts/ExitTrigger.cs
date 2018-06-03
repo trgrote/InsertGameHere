42761565570e1e024b70b2d22855b4d9c94f05ce
Taylor Grote <grotese@gmail.com>
2018-06-03 12:07:25 -0500

    Add Exit Prefabs

---
+++ Assets/Scripts/ExitTrigger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ExitTrigger : MonoBehaviour 
+{
+	[SerializeField, TagSelector] string npcTag;
+
+	void OnTriggerEnter(Collider other)
+	{
+		// If an npc runs into us and is in a leaving state, then destroy them
+		if (other.tag == npcTag)
+		{
+			var data = other.GetComponent<NPCData>();
+
+			if (data == null) return;
+
+			if (data._sm.IsInState(NPCData.eState.Leaving))
+				Destroy(other.gameObject);
+		}
+	}
+}
