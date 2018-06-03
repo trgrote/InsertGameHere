51274e1a8ae77465dbfe37ba55ebec0b84a1a1d9
Taylor Grote <grotese@gmail.com>
2018-06-03 16:43:44 -0500

    Add DieAfterTime Script

---
+++ Assets/Scripts/DieAfterTime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieAfterTime : MonoBehaviour 
+{
+
+	[SerializeField] float timeTillDeath = 10f;
+
+	// Use this for initialization
+	IEnumerator Start () 
+	{
+		yield return new WaitForSeconds(timeTillDeath);
+		Destroy(gameObject);
+	}
+}
