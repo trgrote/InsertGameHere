47b126a2b008b5cec1afa15c210af2750a650fac
Taylor Grote <grotese@gmail.com>
2018-06-03 16:30:49 -0500

    Add Camera Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Simple Camera Controller that just moves the camera left or right
+public class CameraController : MonoBehaviour 
+{
+	void Update()
+	{
+		var horiz = Input.GetAxis("Horizontal");
+		if (Mathf.Abs(horiz) > 0)
+		{
+			transform.position += horiz * transform.right * 2;
+		}
+	}
+}
