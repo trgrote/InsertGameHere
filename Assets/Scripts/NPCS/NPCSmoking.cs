b0bdcbb7b94e386692eb740ed10d9d7659be0557
Price Comstock <price.comstock@gmail.com>
2018-06-03 15:59:20 -0500

    i hope I didn't break more shit

---
+++ Assets/Scripts/NPCS/NPCSmoking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCSmoking : MonoBehaviour {
+
+	[SerializeField] ParticleSystem smoke;
+	private NPCData data;
+
+	// Use this for initialization
+	void Start () {
+		data = transform.parent.GetComponent<NPCData>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (smoke.isPlaying) {
+			if (data.burnLevel < 80f) {
+				smoke.Pause();
+			}
+		}
+	}
+}
