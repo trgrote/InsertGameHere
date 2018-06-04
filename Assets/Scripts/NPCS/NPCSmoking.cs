8d53f441a938f6b8edc8820ebbc2c1de96d94c5a
Price Comstock <price.comstock@gmail.com>
2018-06-03 20:56:09 -0500

    slightly better smoking thresholds

--- Assets/Scripts/NPCS/NPCSmoking.cs
+++ Assets/Scripts/NPCS/NPCSmoking.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class NPCSmoking : MonoBehaviour {
 
 	[SerializeField] ParticleSystem smoke;
-	[SerializeField] [Range(0f,100f)] float smokeBurnThreshold = 80f;
+	[SerializeField] [Range(0f,100f)] float burnLevelThreshold = 80f;
+	[SerializeField] float burnRateThreshold = 5f;
 	private NPCData data;
+	private NPCTanningBehavior tanData;
 
 	// Use this for initialization
 	void Start () {
 		data = transform.parent.GetComponent<NPCData>();
+		tanData = transform.parent.GetComponent<NPCTanningBehavior>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
 		if (smoke.isPlaying) {
-			if (data.burnLevel < smokeBurnThreshold) {
+			if (data.burnLevel < burnLevelThreshold || tanData.burnRate < burnRateThreshold) {
 				smoke.Stop();
 			}
 		} else {
-			if (data.burnLevel >= smokeBurnThreshold) {
+			if (data.burnLevel >= burnLevelThreshold && tanData.burnRate >= burnRateThreshold) {
 				smoke.Play();
 			}
 		}
