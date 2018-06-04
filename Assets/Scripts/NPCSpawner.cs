e60ca05f8ddc8edc4c82b7a05d0a1708b74c2b93
Price Comstock <price.comstock@gmail.com>
2018-06-03 22:28:45 -0500

    Added staggered and speed increasing NPC spawns

--- Assets/Scripts/NPCSpawner.cs
+++ Assets/Scripts/NPCSpawner.cs
@@ -8,8 +8,15 @@ public class NPCSpawner : MonoBehaviour
 	// Use this for initialization
 	[SerializeField] GameObject prefab;
 
-	[SerializeField] float timeBetweenSpawn = 10f;
+	[SerializeField] float startMinTimeBetweenSpawn = 10f;
+	[SerializeField] float startMaxTimeBetweenSpawn = 15f;
 
+	[SerializeField] [Range(0,1)] float spawnTimeDecay = 0.02f;
+
+	
+	private float minTimeBetweenSpawn;
+	private float maxTimeBetweenSpawn;
+	
 	public void Spawn()
 	{
 		Instantiate(prefab, transform.position, Quaternion.identity);
@@ -17,6 +24,8 @@ public class NPCSpawner : MonoBehaviour
 
 	void Start()
 	{
+		minTimeBetweenSpawn = startMinTimeBetweenSpawn;
+		maxTimeBetweenSpawn = startMaxTimeBetweenSpawn;
 		StartCoroutine(ConstantSpawn());
 	}
 
@@ -25,7 +34,9 @@ public class NPCSpawner : MonoBehaviour
 		while (true)
 		{
 			Spawn();
-			yield return new WaitForSeconds(timeBetweenSpawn);
+			yield return new WaitForSeconds(Random.Range(minTimeBetweenSpawn, maxTimeBetweenSpawn));
+			minTimeBetweenSpawn = minTimeBetweenSpawn * (1 - spawnTimeDecay);
+			maxTimeBetweenSpawn = maxTimeBetweenSpawn * (1 - spawnTimeDecay);
 		}
 	}
 }
