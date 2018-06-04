9e0a64354daee3e7994d1aa10adc91c86a2c0c15
Price Comstock <price.comstock@gmail.com>
2018-06-03 23:00:22 -0500

    finished

---
+++ Assets/Scripts/DingHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using rho;
+using System;
+
+public class DingHandler : MonoBehaviour {
+	NPCData data;
+	
+	[SerializeField] AudioClip dingNoise;
+	[SerializeField] AudioSource source;
+
+	void Awake()
+	{
+		data = GetComponent<NPCData>();
+		GlobalEventHandler.Register<NPCLeft>(OnNPCLeft);
+	}
+
+    private void OnNPCLeft(NPCLeft evt)
+    {
+        if (evt.onFire == false) {
+			source.PlayOneShot(dingNoise);
+		}
+    }
+}
